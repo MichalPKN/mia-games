eef6b65bef85c982dac5e03d326873db79058504
MichalPKN <michal.pokorny24@gmail.com>
2022-04-07 23:22:05 +0200

    script update

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         // Typical action to be performed when the document is ready:
-        document.getElementById("game").innerHTML = xhttp.responseText;
+        console.log(xhttp.responseText);
     }
 };
 xhttp.open("GET", "app/database.php", true);
