9fdccf1d4612acf8cc99c3ebae5637dfc282fe34
MichalPKN <michal.pokorny24@gmail.com>
2022-04-13 10:06:50 +0200

    js update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 console.log("cool games");
 const xhttp = new XMLHttpRequest();
 const games = document.getElementById("games");
-const body = document.body;
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var object = JSON.parse(xhttp.responseText);
@@ -10,11 +9,22 @@ xhttp.onreadystatechange = function() {
             data.push(object[i]);
         }
         console.log(data);
-        var div = document.createElement("div");
-        div.className = "game";
-        div.innerText = "bruhhhhhhhhh";
-        console.log(div);
-        games.append(div);
+        for (let game of data) {
+            console.log(game)
+            var div = document.createElement("div");
+            div.className = "game";
+            var h2 = document.createElement("h2");
+            h2.innerText = game["name"];
+            var p = document.createElement("p");
+            p.innerText = game["description"];
+            div.append(h2, p)
+            if (game["img"]) {
+                var img = document.createElement("img");
+                img.src = "img/" + game["img"];
+                div.append(img)
+            }
+            games.append(div);
+        }
 
 
     }
