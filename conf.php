ad1492c3e6167c05b11b24924201186c8b7be91d
MichalPKN <michal.pokorny24@gmail.com>
2022-04-18 16:02:07 +0200

    changed folder structure

--- conf.php
+++ conf.php
@@ -3,5 +3,5 @@
 define("URL", "localhost");
 define("URL_DIR", "/project/");
 define("APP_DIR", "app/");
-define("DATA_DIR", "data/");
+define("DATA_DIR", "data/pages/");
 define("TMPLT_DIR", "templates/");
