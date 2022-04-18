e0d54cd62ed432bda89714c01b28e936b79aae2f
MichalPKN <michal.pokorny24@gmail.com>
2022-04-18 19:38:49 +0200

    updated and added more games to database

--- database/games.sql
+++ database/games.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Apr 07, 2022 at 07:17 PM
--- Server version: 5.7.31
--- PHP Version: 7.3.21
+-- Host: sql5.webzdarma.cz:3306
+-- Generation Time: Apr 18, 2022 at 07:06 PM
+-- Server version: 8.0.26-17
+-- PHP Version: 7.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -27,24 +28,45 @@ SET time_zone = "+00:00";
 -- Table structure for table `games`
 --
 
-DROP TABLE IF EXISTS `games`;
-CREATE TABLE IF NOT EXISTS `games` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+USE `games`;
+
+CREATE TABLE `games` (
+  `id` int UNSIGNED NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
-  `img` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+  `img` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `games`
 --
 
 INSERT INTO `games` (`id`, `name`, `description`, `img`) VALUES
-(1, 'Knights Adventure', 'Top down dungeon crawler', 'knights_adventure'),
-(2, 'Neco-Arc wire crimes', 'guren nyaaaaaaa', 'necoarc.png'),
-(3, 'Poro jump', '2D arcade platformer', '2D-poro.png'),
-(4, 'Bruh Moment', 'Bruh sound effect simulator', '');
+(1, 'Knights Adventure', 'Top down dungeon crawler', 'knights_adventure.png'),
+(2, 'Neco-Arc wire crimes', 'platformer where you as a cat try to destroy the world', 'necoarc.png'),
+(3, 'Flappy Godot', 'flap as the godot engine logo!', 'flappy_godot.png'),
+(4, 'Poro jump', 'jump as high as you can, play as different poros', 'poro_jump.png'),
+(5, 'Shooter mania', 'top down infinite shooter', 'shooter_mania.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `games`
+--
+ALTER TABLE `games`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `games`
+--
+ALTER TABLE `games`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
